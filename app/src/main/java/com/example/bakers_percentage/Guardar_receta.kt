bfbc3de8bebe897285f869112c62d743ef98b559
IgnacioJGE <ijgarciaest@gmail.com>
2025-04-24 21:21:31 +0200

    Pequeñas funcionalidades

--- app/src/main/java/com/example/bakers_percentage/Guardar_receta.kt
+++ app/src/main/java/com/example/bakers_percentage/Guardar_receta.kt
@@ -1,6 +1,11 @@
 package com.example.bakers_percentage
 
+import com.google.android.material.snackbar.Snackbar
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,11 +15,32 @@ class Guardar_receta : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_guardar)
+        setContentView(R.layout.activity_guardar_receta)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val atras= findViewById<Button>(R.id.atras)
+        val addboton= findViewById<Button>(R.id.add_button)
+        val contenedoringredientes= findViewById<LinearLayout>(R.id.contenedor_ingradientes)
+        atras.setOnClickListener{finish()}
+        addboton.setOnClickListener{
+            addingradiente(contenedoringredientes)
+        }
     }
+
+    private fun addingradiente(contendor:LinearLayout){
+        val elementos_internos =contendor.childCount
+        if(elementos_internos<10) {
+            val inflater = LayoutInflater.from(this)
+            val nuevoingrediente = inflater.inflate(R.layout.elemento_ingrediente, contendor, false)
+            contendor.addView(nuevoingrediente, elementos_internos - 1)
+        }else{
+            val rootView: View = findViewById(android.R.id.content)
+            Snackbar.make(rootView, "Too many ingredients", Snackbar.LENGTH_SHORT)
+                .show()
+        }
+        }
+
 }
--- app/src/main/java/com/example/bakers_percentage/Home_Recetas.kt
+++ app/src/main/java/com/example/bakers_percentage/Home_Recetas.kt
@@ -1,32 +1,32 @@
 package com.example.bakers_percentage
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.app.AppCompatActivity
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Home_Recetas.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class Home_Recetas : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
     }
 
     override fun onCreateView(
@@ -36,17 +36,14 @@ class Home_Recetas : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home__recetas, container, false)
     }
-
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val botonaddreceta = view.findViewById<AppCompatButton>(R.id.add_receta)
+        botonaddreceta.setOnClickListener {
+            addreceta()
+        }
+    }
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Home_Recetas.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             Home_Recetas().apply {
@@ -56,4 +53,10 @@ class Home_Recetas : Fragment() {
                 }
             }
     }
+
+    private fun addreceta(){
+        val intent = Intent(requireContext(), Guardar_receta::class.java)
+        startActivity(intent)
+    }
+
 }
